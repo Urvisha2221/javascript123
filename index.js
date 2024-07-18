5b192b9c0315d113d6d028c8fedfcdc58693b96a
Urvisha2221 <urvishadonda39@gmail.com>
2024-07-18 14:54:43 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = 4000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.get('/twitter',(req,res)=>
+{
+    res.send('login page')
+})
+
+app.get('/login',(req,res)=>
+{
+   res.send("<h1>plase login in employee management system</h1>")
+})
+
+app.get('/ems',(req,res)=>
+{
+   res.send("<h2>employee managment syastem</h2>")
+})
+app.listen(process.env.port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
